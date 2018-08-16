40b9d851389f19b9ff8417a795b54e334b21eeaf
Nuril <Nuril@LAPTOP-R1AG4U4Q>
2018-08-16 21:03:14 +0700

    Coba method compareTo & compareToIgnoreCase.

--- Jobsheet11/src/j11/tugas.java
+++ Jobsheet11/src/j11/tugas.java
@@ -39,57 +39,6 @@ public class tugas {
                 + x.equals(r));
         System.out.println("isi x sama dengan r (Not Case Sensitive) : "
                 + x.equalsIgnoreCase(r));
-        
-        System.out.println();
-        
-        System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
-        System.out.println("perbandingan isi x dengan z (Case Sensitive) : "
-                + x.compareTo(z));
-        System.out.println("perbandingan isi x dengan r (Case Sensitive) : "
-                + x.compareTo(r));
-        System.out.println("perbandingan isi x dengan r (Not Case Sensitive) : "
-                + x.compareToIgnoreCase(r));
-        
-        String s = "operasi";
-        System.out.println("isi variable s : "+ s);
-        System.out.println("perbandingan isi r dengan s (Case Sensitive)"
-                + r.compareTo(s));
-        
-        System.out.println();
-        
-        System.out.println("pada x terdapat huruf 'a' : " + x.contains("a"));
-        
-        System.out.println("isi variable x besar semua : "+ x.toUpperCase());
-        System.out.println("isi variable x kecil semua : "+ x.toUpperCase());
-        
-        String t = "  operasi string  ";
-        System.out.println("isi variable t : \""+ t +"\"");
-        System.out.println("isi variable t tanpa spasi : \""+ t.trim());
-        
-        System.out.println("gabungkan isi variable x dan r : "+ x.concat(r));
-       
-        System.out.println();
-        
-        System.out.println("karakter mulai posisi 3 di variable x : "
-                + x.substring(3));
-        
-        System.out.println("karakter dari posisi 3-5 di variable x " 
-                + x.substring(3,6));
-        
-        String tclean = t.trim();
-        System.out.println("isi variable tclean : "+ t.trim());
-        System.out.println("rubah 'i' jadi 'E' di variable tclean : "
-                + tclean.replace("i","E"));
-        
-        String[] arrT = tclean.split(" ");
-        System.out.println("pecah kata di variable tclean : ");
-        for(int i=0;i<arrT.length;i++)
-            System.out.println(arrT[i]);
-        
-        String[] arrTr = tclean.split("r");
-        System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
-        for (int i = 0; i < arrTr.length; i++) 
-            System.out.println(arrTr[i]);
         }
     }
 
