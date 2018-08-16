dd61c68056516e63d99c18937aff26927b1574a7
Nuril <Nuril@LAPTOP-R1AG4U4Q>
2018-08-16 21:01:00 +0700

    Coba method contains, toUpperCase, toLowerCase, trim & concat.

--- Jobsheet11/src/j11/tugas.java
+++ Jobsheet11/src/j11/tugas.java
@@ -90,14 +90,6 @@ public class tugas {
         System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
         for (int i = 0; i < arrTr.length; i++) 
             System.out.println(arrTr[i]);
-        
-        System.out.println();
-        
-        double d = 1545454000;
-        System.out.println(String.format("Uang saya %,.4f", d));
-        
-        System.out.println(String.format(Locale.forLanguageTag("in-ID"),
-                "uang saya Rp %,.2f",d));
         }
     }
 
